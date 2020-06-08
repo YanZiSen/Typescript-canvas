b2b384afa6e306b648793fca2c2f2f95b58cc31f
严宁宁 <yanningning@jiuqi.com.cn>
2020-06-08 19:06:28 +0800

    2020/06/08

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,10 +1,27 @@
 const merge = require('webpack-merge')
 const webpack = require('webpack')
 const baseConfig = require('./webpack.base')
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+const path = require('path')
+
 
 const webpackConfig = merge(baseConfig, {
     plugins: [
-        new webpack.HotModuleReplacementPlugin()
+        new webpack.HotModuleReplacementPlugin(),
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, '../static'),
+                    to: path.resolve(__dirname, '../dist/static'),
+                    globOptions: {
+                        ignore: [
+                          // Ignore all `txt` files
+                          '.*',
+                        ],
+                    },
+                }
+            ]
+        })
     ],
     mode: "development",
     devtool: 'cheap-module-eval-source-map',
